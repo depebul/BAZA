d3f96284724c3964660db96fba7d27e512f41e69
lythx <lythxxx@gmail.com>
2024-11-23 19:46:43 +0100

    orders

--- creation_of_database.sql
+++ creation_of_database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [User]
+CREATE TABLE Users
 (
     UserID          INT PRIMARY KEY,
     UserName        VARCHAR(50)  NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE [User]
     UserPostalCode  VARCHAR(10)
 );
 
-CREATE TABLE Language
+CREATE TABLE Languages
 (
     LanguageID   INT PRIMARY KEY,
     LanguageName VARCHAR(50) NOT NULL
@@ -29,69 +29,69 @@ CREATE TABLE Language
 -- );
 
 
-CREATE TABLE EmployeeType
+CREATE TABLE EmployeeTypes
 (
     EmployeeTypeID   INT PRIMARY KEY,
     EmployeeTypeName VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Employee
+CREATE TABLE Employees
 (
     UserID         INT,
     EmployeeID     INT PRIMARY KEY,
     EmployeeTypeID INT,
     HireDate       DATE NOT NULL,
     Salary         MONEY NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES [User] (UserID),
-    FOREIGN KEY (EmployeeTypeID) REFERENCES EmployeeType (EmployeeTypeID)
+    FOREIGN KEY (UserID) REFERENCES Users (UserID),
+    FOREIGN KEY (EmployeeTypeID) REFERENCES EmployeeTypes (EmployeeTypeID)
 );
 
-CREATE TABLE TeachingLanguage(
+CREATE TABLE TeachingLanguages(
     EmployeeID INT,
     LanguageID INT,
     PRIMARY KEY (EmployeeID, LanguageID),
-    FOREIGN KEY (EmployeeID) REFERENCES Employee (EmployeeID),
-    FOREIGN KEY (LanguageID) REFERENCES Language (LanguageID)
+    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID),
+    FOREIGN KEY (LanguageID) REFERENCES Languages (LanguageID)
 )
 
-CREATE TABLE Student
+CREATE TABLE Students
 (
     UserID     INT,
     StudentID  INT PRIMARY KEY,
-    FOREIGN KEY (UserID) REFERENCES [User] (UserID)
+    FOREIGN KEY (UserID) REFERENCES Users (UserID)
 )
 
-CREATE TABLE StudyingLanguage(
+CREATE TABLE StudyingLanguages(
     StudentID INT,
     LanguageID INT,
     PRIMARY KEY (StudentID, LanguageID),
-    FOREIGN KEY (StudentID) REFERENCES Student (StudentID),
-    FOREIGN KEY (LanguageID) REFERENCES Language (LanguageID)
+    FOREIGN KEY (StudentID) REFERENCES Students (StudentID),
+    FOREIGN KEY (LanguageID) REFERENCES Languages (LanguageID)
 )
 
-CREATE TABLE TranslatingLanguage(
+CREATE TABLE TranslatingLanguages(
     EmployeeID INT,
     LanguageID INT,
     PRIMARY KEY (EmployeeID, LanguageID),
-    FOREIGN KEY (EmployeeID) REFERENCES Employee (EmployeeID),
-    FOREIGN KEY (LanguageID) REFERENCES Language (LanguageID)
+    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID),
+    FOREIGN KEY (LanguageID) REFERENCES Languages (LanguageID)
 )
 
-CREATE TABLE Webinar(
+CREATE TABLE Webinars(
     WebinarID INT PRIMARY KEY,
     WebinarName VARCHAR(100) NOT NULL,
     WebinarStartDate DATE NOT NULL,
     WebinarEndDate DATE NOT NULL,
     WebinarDescription VARCHAR(500),
     WebinarLanguageID INT,
-    WebinarPrice MONEY NOT NULL,
+    WebinarPrice MONEY,
     WebinarTranslatorID INT,
     WebinarTeacherID INT,
     WebinarMeetingLink VARCHAR(100),
     WebinarAccessDateEnd DATE NOT NULL,
-    FOREIGN KEY (WebinarLanguageID) REFERENCES Language (LanguageID),
-    FOREIGN KEY (WebinarTranslatorID) REFERENCES Employee (EmployeeID),
-    FOREIGN KEY (WebinarTeacherID) REFERENCES Employee (EmployeeID)
+    FOREIGN KEY (WebinarLanguageID) REFERENCES Languages (LanguageID),
+    FOREIGN KEY (WebinarTranslatorID) REFERENCES Employees (EmployeeID),
+    FOREIGN KEY (WebinarTeacherID) REFERENCES Employees (EmployeeID)
 )
 
 CREATE Table StudentBoughtWebinars(
@@ -100,8 +100,8 @@ CREATE Table StudentBoughtWebinars(
     WebinarBoughtDate DATE NOT NULL,
     WebinarAccessDateEnd DATE NOT NULL,
     PRIMARY KEY (StudentID, WebinarID),
-    FOREIGN KEY (StudentID) REFERENCES Student (StudentID),
-    FOREIGN KEY (WebinarID) REFERENCES Webinar (WebinarID)
+    FOREIGN KEY (StudentID) REFERENCES Students (StudentID),
+    FOREIGN KEY (WebinarID) REFERENCES Webinars (WebinarID)
 )
 
 CREATE TABLE Cities(
@@ -113,7 +113,7 @@ CREATE TABLE UserAvailableCities(
     UserID INT,
     CityID INT,
     PRIMARY KEY (UserID, CityID),
-    FOREIGN KEY (UserID) REFERENCES [User] (UserID),
+    FOREIGN KEY (UserID) REFERENCES Users (UserID),
     FOREIGN KEY (CityID) REFERENCES Cities (CityID)
 )
 
@@ -126,15 +126,15 @@ CREATE TABLE Courses(
     CourseDescription VARCHAR(500),
     CourseLanguageID INT,
     CoursePrice MONEY NOT NULL,
-    FOREIGN KEY (CourseLanguageID) REFERENCES Language (LanguageID),
+    FOREIGN KEY (CourseLanguageID) REFERENCES Languages (LanguageID),
 )
 
-CREATE TABLE CourseModuleMeetingType(
+CREATE TABLE CourseModuleMeetingTypes(
     CourseModuleMeetingTypeID INT PRIMARY KEY,
     CourseModuleMeetingTypeName VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE CourseModule(
+CREATE TABLE CourseModules(
     CourseID INT,
     ModuleID INT PRIMARY KEY,
     ModuleName VARCHAR(100) NOT NULL,
@@ -143,16 +143,16 @@ CREATE TABLE CourseModule(
     CourseModuleMeetingTypeID INT,
     CourseModuleLessonQuantity INT NOT NULL,
     FOREIGN KEY (CourseID) REFERENCES Courses (CourseID),
-    FOREIGN KEY (ModuleLanguageID) REFERENCES Language (LanguageID),
-    FOREIGN KEY (CourseModuleMeetingTypeID) REFERENCES CourseModuleMeetingType (CourseModuleMeetingTypeID)
+    FOREIGN KEY (ModuleLanguageID) REFERENCES Languages (LanguageID),
+    FOREIGN KEY (CourseModuleMeetingTypeID) REFERENCES CourseModuleMeetingTypes (CourseModuleMeetingTypeID)
 )
 
-CREATE TABLE CourseLessonMeetingType(
+CREATE TABLE CourseLessonMeetingTypes(
     CourseLessonMeetingTypeID INT PRIMARY KEY,
     CourseLessonMeetingTypeName VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE CourseLesson(
+CREATE TABLE CourseLessons(
     ModuleID INT,
     LessonID INT,
     CourseID INT,
@@ -165,9 +165,70 @@ CREATE TABLE CourseLesson(
     LessonMeetingDateEnd DATE NOT NULL,
     LessonCapacity INT,
     CourseLessonMeetingTypeID INT,
+    IsTranslatedToPolish BIT,
+    TranslatorID INT,
     FOREIGN KEY (ModuleID) REFERENCES CourseModule (ModuleID),
     FOREIGN KEY (CourseID) REFERENCES Courses (CourseID),
     FOREIGN KEY (LessonLanguageID) REFERENCES Language (LanguageID),
     FOREIGN KEY (CourseLessonMeetingTypeID) REFERENCES CourseLessonMeetingType (CourseLessonMeetingTypeID)
+    FOREIGN KEY (TranslatorID) REFERENCES Employees (EmployeeID),
+)
+
+CREATE TABLE CourseModulesPassed(
+    StudentID INT,
+    ModuleID INT,
+    FOREIGN KEY (ModuleID) REFERENCES CourseModules (ModuleID),
+    FOREIGN KEY (StudentID) REFERENCES Students (StudentID)
+)
+
+CREATE TABLE CourseLessonsPassed(
+    StudentID INT,
+    LessonID INT,
+    ModuleID INT,
+    FOREIGN KEY (LessonID) REFERENCES CourseLessons (LessonID),
+    FOREIGN KEY (ModuleID) REFERENCES CourseModules (ModuleID),
+    FOREIGN KEY (StudentID) REFERENCES Students (StudentID)
+)
+
+CREATE TABLE WebinarsPassed(
+    StudentID INT,
+    ModuleID INT,
+    FOREIGN KEY (WebinarID) REFERENCES Webinars (WebinarID),
+    FOREIGN KEY (StudentID) REFERENCES Students (StudentID)
+)
+
+CREATE TABLE Orders(
+    OrderID INT PRIMARY KEY,
+    OrderDate DATE NOT NULL,
+    OrderPaymentLink VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE OrderDetails(
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT,
+    AmountPaid MONEY NOT NULL,
+    PaidDate DATE,
+    PostponementDate DATE
+)
+
+CREATE TABLE OrderWebinars(
+    WebinarID INT PRIMARY KEY,
+    OrderDetailID INT,
+    FOREIGN KEY (OrderDetailID) REFERENCES OrderDetails (OrderDetailID),
+    FOREIGN KEY (WebinarID) REFERENCES Webinars (WebinarID)
+)
+
+CREATE TABLE OrderStudies(
+    StudiesID INT PRIMARY KEY,
+    OrderDetailID INT,
+    FOREIGN KEY (OrderDetailID) REFERENCES OrderDetails (OrderDetailID),
+    FOREIGN KEY (StudiesID) REFERENCES Studies (StudiesID)
+)
+
+CREATE TABLE OrderCourses(
+    CourseID INT PRIMARY KEY,
+    OrderDetailID INT,
+    FOREIGN KEY (OrderDetailID) REFERENCES OrderDetails (OrderDetailID),
+    FOREIGN KEY (CourseID) REFERENCES Courses (CourseID)
 )
 
