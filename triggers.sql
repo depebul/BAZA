2ffdde75965ce575e0ede61fb0c0e37695f075e9
lythx <lythxxx@gmail.com>
2025-01-04 11:08:06 +0100

    webinar payment trigger

---
+++ triggers.sql
@@ -0,0 +1,77 @@
+CREATE TRIGGER HandleWebinarPayment
+ON OrderDetails
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT Orders.UserID, Webinars.WebinarID
+        FROM inserted
+        INNER JOIN Orders
+        ON Orders.OrderID = inserted.OrderID
+        INNER JOIN OrderWebinars
+        ON OrderWebinars.OrderDetailID = inserted.OrderDetailID
+        INNER JOIN Webinars
+        ON Webinars.WebinarID=OrderWebinars.WebinarID
+        WHERE inserted.AmountPaid = inserted.AmountToPay
+        AND WebinarStartDate > GETDATE()
+        AND Orders.UserID IN (
+            SELECT DISTINCT StudentID FROM StudentBoughtWebinars
+            )
+       )
+    BEGIN
+        RAISERROR('Student already bought that webinar', 1, 1)
+    END
+    ELSE
+    BEGIN
+        INSERT INTO StudentBoughtWebinars(StudentID, WebinarID, WebinarBoughtDate, WebinarAccessDateEnd)
+        (SELECT Orders.UserID as StudentID, Webinars.WebinarID, GETDATE() as WebinarBoughtDate, DATEADD(DAY, 30, GETDATE()) as WebinarAccessDateEnd
+        FROM inserted
+        INNER JOIN Orders
+        ON Orders.OrderID = inserted.OrderID
+        INNER JOIN OrderWebinars
+        ON OrderWebinars.OrderDetailID = inserted.OrderDetailID
+        INNER JOIN Webinars
+        ON Webinars.WebinarID=OrderWebinars.WebinarID
+        WHERE inserted.AmountPaid = inserted.AmountToPay
+        AND WebinarStartDate > GETDATE())
+    END
+END;
+
+-- CREATE TRIGGER HandleCoursePayment
+-- ON OrderDetails
+-- AFTER INSERT, UPDATE
+-- AS
+-- BEGIN
+--     IF EXISTS (
+--         SELECT Orders.UserID, Webinars.WebinarID
+--         FROM inserted
+--         INNER JOIN Orders
+--         ON Orders.OrderID = inserted.OrderID
+--         INNER JOIN OrderWebinars
+--         ON OrderWebinars.OrderDetailID = inserted.OrderDetailID
+--         INNER JOIN Webinars
+--         ON Webinars.WebinarID=OrderWebinars.WebinarID
+--         WHERE inserted.AmountPaid = inserted.AmountToPay
+--         AND WebinarStartDate > GETDATE()
+--         AND Orders.UserID IN (
+--             SELECT DISTINCT StudentID FROM StudentBoughtWebinars
+--             )
+--        )
+--     BEGIN
+--         RAISERROR('Student already bought that webinar', 1, 1)
+--     END
+--     ELSE
+--     BEGIN
+--         INSERT INTO StudentBoughtWebinars(StudentID, WebinarID, WebinarBoughtDate, WebinarAccessDateEnd)
+--         (SELECT Orders.UserID as StudentID, Webinars.WebinarID, GETDATE() as WebinarBoughtDate, DATEADD(DAY, 30, GETDATE()) as WebinarAccessDateEnd
+--         FROM inserted
+--         INNER JOIN Orders
+--         ON Orders.OrderID = inserted.OrderID
+--         INNER JOIN OrderWebinars
+--         ON OrderWebinars.OrderDetailID = inserted.OrderDetailID
+--         INNER JOIN Webinars
+--         ON Webinars.WebinarID=OrderWebinars.WebinarID
+--         WHERE inserted.AmountPaid = inserted.AmountToPay
+--         AND WebinarStartDate > GETDATE())
+--     END
+-- END
